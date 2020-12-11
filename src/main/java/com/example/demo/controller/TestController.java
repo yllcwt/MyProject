030c0416ebbe0ecda429fe2103d222588448037f
yllcwt <2724059421@qq.com>
2020-12-11 18:20:28 +0800

    '新分支'

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,8 +1,12 @@
 package com.example.demo.controller;
 
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class TestController {
@@ -15,5 +19,12 @@ public class TestController {
     @GetMapping("update")
     public String update() {
         return "update";
+    }
+    @GetMapping("delete")
+    public String delete(Model model){
+        Map<String, Integer> map = new HashMap<>();
+        map.put("a",2);
+        model.addAllAttributes(map);
+        return "delete";
     }
 }
